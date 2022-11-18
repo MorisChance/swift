9054a1a2856660d4fadf7dbaf813d52f963f28cb
Moris8181 <moriskousuke8181@gmail.com>
2022-11-19 08:47:52 +0900

    Initial Commit

---
+++ Academia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Academia
+//
+//  Created by 森本　浩輔 on 2022/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
