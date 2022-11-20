f8f8f5474dbaaa810df61e6047e06443426823b6
Moris8181 <moriskousuke8181@gmail.com>
2022-11-20 17:37:20 +0900

    完成

--- Academia/ViewController.swift
+++ Academia/ViewController.swift
@@ -4,16 +4,16 @@
 //
 //  Created by 森本　浩輔 on 2022/11/19.
 //
-
 import UIKit
-
+import WebKit
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+  @IBOutlet weak var webview: WKWebView!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    if let url = URL(string: "https://academia-2022.herokuapp.com/welcome") {
+      let request = NSURLRequest(url: url as URL)
+      webview.load(request as URLRequest)
+    // Do any additional setup after loading the view.
+  }
 }
-
+}
